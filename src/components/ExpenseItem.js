2a6ec4db75af2ba6c54ac5854699c1abc8bc01cc
Rushikesh Ghosalkar <rushighosalkar@gmail.com>
2021-05-11 18:31:19 +0530

    First Initialize

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ExpenseItem.css';
+
+function Expense(props) {
+    const expenseTitle = props.title;
+    const expensePrice = props.amount;
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.date.getFullYear();
+
+    return (
+        <div className = "expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className = "expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className = "expense-item__price">${expensePrice}</div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Expense;
