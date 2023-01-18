478aa91eca134e6f42859bb1fac352f09061aafe
Rushikesh Ghosalkar <rushighosalkar@gmail.com>
2023-01-18 17:27:51 +0530

    V1

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,7 @@ import './Chart.css';
 
 const Chart = (props) => {
     const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-    console.log(dataPointValues);
+    //console.log(dataPointValues);
     const totalMaximum = Math.max(...dataPointValues);
     return (
         <div className="chart">
